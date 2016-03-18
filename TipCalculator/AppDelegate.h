026a535c922911eab86940263ba1b5317e446f2a
thomasinyvr <tomfriesman@gmail.com>
2016-03-18 11:20:37 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Thomas Friesman on 2016-03-18.
+//  Copyright © 2016 Thomas Friesman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
